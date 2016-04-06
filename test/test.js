d8fe5946638187a206fedd7cb357bc69ef2d0ae6
g3aster <kevin.doodnauth@hotmail.com>
2016-04-06 16:35:04 -0400

    test.js structural template for mocha

---
+++ test/test.js
@@ -0,0 +1,117 @@
+
+/*
+FriendZone Testing and Structural Layout:
+
+Module: FriendZone
+
+pages:
+-Admin
+-Event
+-Landing
+-Navigation
+-Profile
+-Search
+ */
+
+var assert = require('assert');
+
+var FriendZone = require('./startup.js');
+
+describe('FriendZone tests', function() {
+
+    describe('Admin tests', function() {
+
+        return null;
+        /*//////ADMIN//////*/
+        //function:
+        //function:
+        //function:
+        //function:
+        //var:
+        //var:
+        //var:
+        //var:
+    })
+
+
+    describe('Event tests', function() {
+
+        /*//////EVENT//////*/
+        //function:
+        //function:
+        //function:
+        //function:
+        //var:
+        //var:
+        //var:
+        //var:
+
+
+    })
+
+
+    describe('Landing tests', function(){
+
+        /*//////LANDING//////*/
+        //function:
+        //function:
+        //function:
+        //function:
+        //var:
+        //var:
+        //var:
+        //var:
+
+    })
+
+
+    describe('Navigation tests', function(){
+
+        /*//////NAVIGATION//////*/
+        //function:
+        //function:
+        //function:
+        //function:
+        //var:
+        //var:
+        //var:
+        //var:
+
+    })
+
+
+    describe('Profile tests', function (){
+
+        /*//////PROFILE//////*/
+        //function:
+        //function:
+        //function:
+        //function:
+        //var:
+        //var:
+        //var:
+        //var:
+
+
+    })
+
+
+    describe('Search tests', function(){
+
+        /*//////SEARCH//////*/
+        //function:
+        //function:
+        //function:
+        //function:
+        //var:
+        //var:
+        //var:
+        //var:
+
+    })
+
+
+
+
+
+})
